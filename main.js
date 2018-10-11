12350b651e013a7cd783f06bd62aab7b75cc8f5f
sakamotomitsu <sakamotomitsuki000@gmail.com>
2018-10-11 16:11:33 +0900

    first

---
+++ main.js
@@ -0,0 +1,42 @@
+var app = new Vue({
+  el: '#app',
+
+  data: {
+    message: 'Hello Vue.js!',
+    msg: '初期メッセージ',
+    show: true,
+    list: ['りんご', 'ばなな', 'いちご'],
+    count: 0,
+    isChild: true,
+    isActive: true,
+    textColor: 'red',
+    bgColor: 'lightgray',
+    classObject: {
+      child: true,
+      'is-active': false
+    },
+    styleObject: {
+      color: 'red',
+      backgroundColor: 'lightgray'
+    },
+    item: {
+      id: 1,
+      src: 'item1.jpg',
+      width: 200,
+      height: 200
+    },
+    radius: 50,
+    ok: true,
+    ng: false
+  },
+
+  methods: {
+    handleClick: function (event) {
+      alert(event.target)
+    },
+    increment: function () {
+      this.count += 1
+    }
+  }
+
+})
