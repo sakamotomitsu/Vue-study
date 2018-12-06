8d97916055c13e7f46fdba200eff711f15f5b074
sakamotomitsu <sakamotomitsuki000@gmail.com>
2018-12-06 23:57:40 +0900

    リストの絞り込み

--- main.js
+++ main.js
@@ -51,7 +51,16 @@ var app = new Vue({
     scrollY: 0,
     timer: null,
     width: 800,
-    height: 600
+    height: 600,
+    budget: 300,
+    limit: 2,
+    list: [
+      { id: 1, name: 'りんご', price: 100 },
+      { id: 2, name: 'ばなな', price: 200 },
+      { id: 3, name: 'いちご', price: 400 },
+      { id: 4, name: 'オレンジ', price: 300 },
+      { id: 5, name: 'メロン', price: 500 },
+    ]
   },
 
   created: function () {
@@ -170,7 +179,16 @@ var app = new Vue({
 
     computedData: function () {
       return Math.random()
-    }
+    },
+
+    matched: function () {
+      return this.list.filter(function (el) {
+        return el.price <= this.budget
+      }, this)
+    },
+     limited: function () {
+       return this.matched.slice(0, this.limit)
+     }
   }
 
 });
