2390091f49f3fc060ec66e60c4f54e80f39ba110
sakamotomitsu <sakamotomitsuki000@gmail.com>
2018-12-15 00:10:34 +0900

    フィルター 複数のフィルタ

--- main.js
+++ main.js
@@ -220,6 +220,15 @@ var app = new Vue({
 
     filter: function (message, foo, num) {
       console.log(message, foo, num);
+    },
+
+    /* 小数点以下を第2位に丸めるフィルタ */
+    round: function (val) {
+      return Math.round(val * 100) / 100
+    },
+    /* 度からラジアンに変換するフィルタ */
+    radian: function (val) {
+      return val * Math.PI / 180
     }
   },
 
