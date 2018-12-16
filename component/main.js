111aac661f00f92457590b64ca3e5b413996cbbb
sakamotomitsu <sakamotomitsuki000@gmail.com>
2018-12-16 23:51:15 +0900

    コンポーネントの定義 - グローバル

--- component/main.js
+++ component/main.js
@@ -1,7 +1,13 @@
+//コンポーネントを定義
 Vue.component('my-component', {
   template: '<p>MyComponent</p>'
 });
 
 var app = new Vue({
   el: '#app',
+
+  components: {
+    //<my-component>がルートでのみ使用可能になる
+    'my-component': myComponent
+  }
 });
