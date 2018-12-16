912d2ae2499f4e17ab3ef9252bb343cfddcbbe06
sakamotomitsu <sakamotomitsuki000@gmail.com>
2018-12-16 23:57:17 +0900

    コンポーネントのオプション

--- component/main.js
+++ component/main.js
@@ -1,6 +1,18 @@
 //コンポーネントを定義
 Vue.component('my-component', {
+  //テンプレート
   template: '<p>MyComponent</p>'
+
+  //データはオブジェクトを返す関数にする
+  data: function () {
+    return{
+      message: 'Hello Vue.js'
+    }
+  },
+  methods: {
+    //メソッドや算出プロパティ、ウォッチャなどの定義方法は
+    //ルートコンストラクタのオプションと同じ
+  }
 });
 
 var app = new Vue({
