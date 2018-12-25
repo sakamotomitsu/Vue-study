1d1cd90f49f7807e30322b87159375d260fac912
sakamotomitsu <sakamotomitsuki000@gmail.com>
2018-12-25 18:56:54 +0900

    コンポーネント間の通信 - propsで受け取ったデータは勝手に書き換えてはいけない

--- component/main.js
+++ component/main.js
@@ -23,9 +23,20 @@ Vue.component('my-component', {
 });
 
 
+// Vue.component('comp-child', {
+//   template: '<li>{{ name }} HP.{{ hp }}</li>',
+//   props: ['name', 'hp']
+// });
 Vue.component('comp-child', {
-  template: '<li>{{ name }} HP.{{ hp }}</li>',
-  props: ['name', 'hp']
+  template: '<li>{{ name }} HP.{{ hp }}\
+  <button v-on:click="doAttack">攻撃する</button></li>',
+  props: ['name', 'hp'],
+  methods: {
+    doAttack: function () {
+      //勝手に攻撃
+      this.hp -= 10
+    }
+  }
 });
 
 
