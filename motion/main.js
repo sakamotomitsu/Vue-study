b0573c067a20e21737b4f74dda2b28764310bc75
sakamotomitsu <sakamotomitsuki000@gmail.com>
2019-01-30 18:53:33 +0900

    トランジション - 移動トランジション

--- motion/main.js
+++ motion/main.js
@@ -2,6 +2,17 @@ var app = new Vue({
   el: '#app',
   data: {
     show: true,
-    count: 0
+    count: 0,
+    order: false,
+    list: [
+      { id: 1, name: 'りんご', price: 100},
+      { id: 2, name: 'ばなな', price: 200},
+      { id: 3, name: 'いちご', price: 300}
+    ]
+  },
+  computed: {
+    sortedList: function () {
+      return _.orderBy(this.list, 'price', this.order ? 'desc' : 'asc')
+    }
   }
 });
