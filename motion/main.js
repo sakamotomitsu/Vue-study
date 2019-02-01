46c8f2566d3119357f005c56b8e53dd97f512dda
sakamotomitsu <sakamotomitsuki000@gmail.com>
2019-02-02 01:12:58 +0900

    トランジション - svgのトランジション

--- motion/main.js
+++ motion/main.js
@@ -1,3 +1,8 @@
+Vue.component('my-circle', {
+  template: '<circle cx="80" cy="75" r="50" v-bind:fill="fill"/>',
+  props: { fill: String }
+});
+
 var app = new Vue({
   el: '#app',
   data: {
@@ -8,11 +13,16 @@ var app = new Vue({
       { id: 1, name: 'りんご', price: 100},
       { id: 2, name: 'ばなな', price: 200},
       { id: 3, name: 'いちご', price: 300}
-    ]
+    ],
+    toggle: false
   },
   computed: {
     sortedList: function () {
       return _.orderBy(this.list, 'price', this.order ? 'desc' : 'asc')
+    },
+
+    fill: function () {
+      return this.toggle ? 'lightpink' : 'skyblue'
     }
   }
 });
