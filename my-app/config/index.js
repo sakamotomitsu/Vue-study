58ced4a0b16d94481da11ef73443bd1e50c5eb39
sakamotomitsu <sakamotomitsuki000@gmail.com>
2019-02-06 13:58:17 +0900

    Vue-cli - パスの統一やクロスドメイン対策

--- my-app/config/index.js
+++ my-app/config/index.js
@@ -65,5 +65,16 @@ module.exports = {
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
+  },
+
+  /* -------- */
+  proxyTable: {
+    '/api': {
+      target: 'http://localhost:8081',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': '/my-app/api'
+      }
+    }
   }
 }
