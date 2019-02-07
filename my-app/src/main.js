6fc8ba7e0a5ee249c59ad943c58ccb734885c7c3
sakamotomitsu <sakamotomitsuki000@gmail.com>
2019-02-07 18:08:01 +0900

    Vueプラグインの自作

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -5,6 +5,48 @@ import App from './App'
 
 Vue.config.productionTip = false
 
+/*
+var windowPlugin = {
+  install: function (Vue) {
+    // プラグインデータ用にVueインスタンスを利用する
+    var store = new Vue({
+      data: { scrollY: 0 }
+    })
+    // ウィンドウのスクロールイベントをハンドル
+    var timer = null
+    window.addEventListener('scroll', function () {
+      if(timer === null){
+        timer = setTimeout(function () {
+          // 200ms間隔でscrollYプロパティに代入
+          store.scrollY = window.scrollY
+          clearTimeout(timer)
+          timer = null
+        }, 200)
+      }
+    })
+
+    //インスタンスプロパティ登録
+    Vue.prototype.$window = store.$data
+  }
+}
+Vue.use(windowPlugin)
+*/
+/*   コンポーネントからは次の用に使用できます
+
+Vue.component('my-component', {
+  template: '<div>{{ scrollY }}</div>',
+  computed: {
+    scrollY: function () {
+      return this.$window.scrollY
+    }
+  }
+})
+*/
+
+
+
+
+
 /* eslint-disable no-new */
 /*new Vue({
   el: '#app',
