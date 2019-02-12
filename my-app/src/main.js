558f64f54e7b841d12912b975e78138e2319f2f6
sakamotomitsu <sakamotomitsuki000@gmail.com>
2019-02-12 10:59:35 +0900

    Vuexをごにょごにょ

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,7 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+//import Vue from 'vue'
+//import App from './App'
+
+
+import 'babel-polyfill'
 import Vue from 'vue'
-import App from './App'
+import Vuex from 'vuex'
+
+// プラグインとして登録
+Vue.use(Vuex)
+
 
 Vue.config.productionTip = false
 
