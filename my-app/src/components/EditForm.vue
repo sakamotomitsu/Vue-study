7fb8a1bc943af5670f66c8368104fb3f9f03c97c
sakamotomitsu <sakamotomitsuki000@gmail.com>
2019-02-17 20:44:56 +0900

    ステートやゲッターにv-modelを使用する

--- my-app/src/components/EditForm.vue
+++ my-app/src/components/EditForm.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="edit-form">
     <input type="text" :value="message" @input="doUpdate">
+    <!--<input v-model="message">-->
   </div>
 </template>
 
@@ -9,6 +10,14 @@
     name: "EditForm",
     computed: {
       message() { return this.$store.getters.message }
+
+      /*
+      // 1つの算出プロパティに入出力をまとめるとスッキリ
+      message :{
+        get() { return this.$store.getters.message },
+        set(value) { this.$store.dispatch('doUpdate', value) }
+      }
+      */
     },
     methods: {
       doUpdate(event) {
