e53385a680dfc6b6c3ab81ad4253f86288d8b091
sakamotomitsu <sakamotomitsuki000@gmail.com>
2019-02-17 16:00:40 +0900

    Vuex - コンポーネント読み込み

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,24 +1,42 @@
 <template>
   <div id="app">
+    <!--
     <img src="./assets/logo.png">
     <HelloWorld/>
+    -->
+    <p>{{ message }}</p>
+    <EditForm/>
   </div>
 </template>
 
 <script>
+/*
 import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  created() {
-    //ストアの状態を取得
-    console.log(this.$store.state.count)
+name: 'App',
+components: {
+EditForm,
+HelloWorld
+},
+created() {
+//ストアの状態を取得
+console.log(this.$store.state.count)
 
-    //ストアの状態を更新
-    this.$store.commit('increment')
+//ストアの状態を更新
+this.$store.commit('increment')
+}
+}
+*/
+
+//子コンポーネントを読み込む
+import EditForm from "@/components/EditForm";
+export default {
+  name: 'app',
+  components: { EditForm },
+  computed: {
+    //ローカルのmessageとストアのmessageを同期
+    message() { return this.$store.getters.message }
   }
 }
 </script>
