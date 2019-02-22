30413febf863d525b161bf0fda86dd5d63a4a2c9
sakamotomitsu <sakamotomitsuki000@gmail.com>
2019-02-23 01:10:36 +0900

    vue-router 動的ページ(view)

---
+++ vue-app/src/views/ProductList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="product-list">
+    <h1>商品一覧</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ProductList"
+  }
+</script>
+
+<style scoped>
+
+</style>
